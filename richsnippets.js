bf2e3bd97e8b5bc5970564ae7c1d906351c7b21d
Michael Schaefers <michael.schaefers@scoop-software.de>
2016-06-23 18:48:07 +0200

    Erste Version

---
+++ richsnippets.js
@@ -0,0 +1,37 @@
+<script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Localbusiness",
+        "url": "http://fstm.de/",
+        "sameAs": "http://www.tischler-schreiner.org/moers/frank-schaefers-tischlermeister-aULE7B",
+        "sameAs": "https://www.moneyhouse.de/FSTM-GmbH-Moers",
+        "sameAs": "http://www.unternehmen24.info/Firmeninformationen/DE/3611526",
+        "sameAs": "http://www.unternehmen1.com/FSTM+GmbH.html",
+        "sameAs": "http://www.genios.de/firmen/firma/FS/fstm-gmbh.html",
+        "sameAs": "http://www.firmenwissen.de/az/firmeneintrag/47445/5210289972/FSTM_GMBH.html",
+        "sameAs": "https://compaly.com/de/unternehmen/fstm-gmbh-moers-de",
+        "sameAs": "http://branchenbuch.meinestadt.de/moers/company/10307863",
+        "sameAs": "http://www.in0.de/fstm.de",
+        "name": "FSTM GmbH",
+        "address": {
+            "@type": "PostalAddress",
+            "email": "frankschaefers@fstm.de",
+            "addressCountry": "DE",
+            "postalCode": "47445",
+            "streetAddress": "Trakehnenstr. 8b",
+            "addressLocality": "Moers",
+            "faxNumber": "+49 2841 400 695",
+            "telephone": "+49 2841 400 694"
+        },
+        "founder": {
+            "@type": "Person",
+            "name": "Frank Schaefers",
+            "givenName": "Frank",
+            "familyName": "Schaefers",
+            "email": "frankschaefers@fstm.de",
+            "gender": "http://schema.org/Male",
+            "jobtitle": "Tischlermeister",
+            "telephone": "+49 163 4397337"
+        }
+    }
+</script>
